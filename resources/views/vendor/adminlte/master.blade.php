d1c97f736da94e3fec7c436141ccedff2e138826
Ali Galip Çamlı <aligalip@camli.info>
2019-01-28 17:15:22 +0300

    Custom CSS file

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -30,6 +30,8 @@
 
     @yield('adminlte_css')
 
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
