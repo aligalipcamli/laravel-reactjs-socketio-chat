a5a142f7a728c7fce36a74f5b7b4f84f8099e103
Ali Galip Çamlı <aligalip@camli.info>
2019-02-03 11:29:46 +0300

    Define controller methods for showing, fetching & persisting messages

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChatsController extends Controller
 {
-    //
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
+
+  /**
+   * Fetch all messages
+   *
+   * @return Message
+   */
+  public function fetchMessages()
+  {
+    return Message::with('user')->get();
+  }
+
+  /**
+   * Persist message to database
+   *
+   * @param  Request $request
+   * @return Response
+   */
+  public function sendMessage(Request $request)
+  {
+    $user = Auth::user();
+
+    $message = $user->messages()->create([
+      'message' => $request->input('message')
+    ]);
+
+    return ['status' => 'Message Sent!'];
+  }
 }
