3f666f6919bdc5d5e0a033e5502c88537fa7ed8d
Ali Galip Çamlı <aligalip@camli.info>
2019-02-03 11:22:04 +0300

    Relationship between User & Message

--- app/Message.php
+++ app/Message.php
@@ -12,4 +12,14 @@ class Message extends Model
    * @var array
    */
   protected $fillable = ['message'];
+
+  /**
+   * A message belong to a user
+   *
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
